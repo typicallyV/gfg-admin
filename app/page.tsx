92f3dbd984db2cbec530a7927e8e408a853477c4
Sarvesh Hardas <hardassarvesh58@gmail.com>
2026-03-28 22:51:31 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+  return (
+    <>
+      <div className="min-h-screen text-center flex items-center justify-center">
+        <h1 className="text-4xl font-bold">GFG Admin Panel</h1>
+      </div>
+    </>
+  )
+}
