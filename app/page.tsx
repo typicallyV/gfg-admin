c0467164e8ebeb4fd1eda6d44121729f7d97b21b
Sarvesh Hardas <hardassarvesh58@gmail.com>
2026-03-30 11:34:09 +0530

    Added separate component for form

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Event from "./dashboard/events/page";
 export default function Home(){
   return (
     <>
-      <div className="min-h-screen text-center flex py-7 px-3 justify-center">
+      <div className="min-h-screen flex py-7 px-3 justify-center">
         <Event/>
       </div>
     </>
