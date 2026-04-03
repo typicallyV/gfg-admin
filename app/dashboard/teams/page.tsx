e3c29fc10056b39a3166e091e6bbe3a07e151d5b
Sarvesh Hardas <hardassarvesh58@gmail.com>
2026-04-03 23:55:32 +0530

    removed heading from teams page

--- app/dashboard/teams/page.tsx
+++ app/dashboard/teams/page.tsx
@@ -5,10 +5,6 @@ const page = () => {
   return (
     <div>
       <TeamNavbar />
-      <div className="p-4">
-        <h2 className="text-xl font-bold">Team Management</h2>
-        <p>Welcome to the Team Management page!</p>
-      </div>
     </div>
   )
 }
